7dadccc4019a3d5d5f6d9b89bfcb453431da9220
sedani.ab <sedaniarjun@gmail.com>
2017-11-03 15:00:19 +0530

    commit1

---
+++ app/src/main/java/com/androidtutorialshub/loginregister/activities/SplashActivity1.java
@@ -0,0 +1,49 @@
+package com.androidtutorialshub.loginregister.activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.androidtutorialshub.loginregister.R;
+
+/**
+ * Created by sedani.ab on 11/2/2017.
+ */
+
+public class SplashActivity1 extends AppCompatActivity {
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash1);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(null != actionBar){
+            actionBar.hide();
+        }
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent startActivityIntent = new Intent(SplashActivity1.this, LoginActivity.class);
+                startActivity(startActivityIntent);
+                SplashActivity1.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
+
