08b954354125c34ec901fc298a2560fae88fae15
sedani.ab <sedaniarjun@gmail.com>
2017-12-06 16:45:09 +0530

    commit1

--- app/src/main/java/com/androidtutorialshub/loginregister/activities/ListViewActivity.java
+++ app/src/main/java/com/androidtutorialshub/loginregister/activities/ListViewActivity.java
@@ -1,15 +1,55 @@
 package com.androidtutorialshub.loginregister.activities;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidtutorialshub.loginregister.R;
+import com.androidtutorialshub.loginregister.charting.PieChart;
 
-public class ListViewActivity extends AppCompatActivity {
+import java.util.List;
 
+public class ListViewActivity  extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Resources res = getResources();
+
         setContentView(R.layout.activity_list_view);
+        final PieChart pie = (PieChart) this.findViewById(R.id.Pie);
+        pie.addItem("Agamemnon", 2, res.getColor(R.color.seafoam));
+        pie.addItem("Bocephus", 3.5f, res.getColor(R.color.chartreuse));
+        pie.addItem("Calliope", 2.5f, res.getColor(R.color.emerald));
+        pie.addItem("Daedalus", 3, res.getColor(R.color.bluegrass));
+        pie.addItem("Euripides", 1, res.getColor(R.color.turquoise));
+        pie.addItem("Ganymede", 3, res.getColor(R.color.slate));
+
+        ((Button) findViewById(R.id.Reset)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                pie.setCurrentItem(0);
+            }
+        });
     }
 }
